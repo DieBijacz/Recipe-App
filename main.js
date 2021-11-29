bc53f3ae4bbbf0447a0cce8863d125e67ebc680b
masta <mastanilewicz@gmail.com>
2021-11-29 01:54:31 +0000

    added modal with recipies of meals

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const meals = document.querySelector('.container')
 const favList = document.querySelector('#fav-list')
 const searchText = document.querySelector('#search-text')
 const searchBtn = document.querySelector('#search')
+const modal = document.querySelector('#modal')
+const modalCloseBtn = document.querySelector('#btn-close-modal')
 
 getRandomMeal()
 fetchFavMeals()
 
-// GET DATA FUNCTIONS
+// ================ GET DATA FUNCTIONS ================
 async function getRandomMeal() {
     const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     const respData = await resp.json();
 
     const randomMeal = respData.meals[0];
-
+    console.log(randomMeal);
     loadRandomMeal(randomMeal, true)
+
 }
 async function getMealById(id) {
     const resp = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+id);
@@ -31,6 +34,7 @@ async function getMealBySearch(term) {
     return meals
 }
 
+// ================ GET DATA FUNCTIONS ================
 // ============ GET RANDOM MEAL ============
 
 function loadRandomMeal(mealData, random = false) {
@@ -46,20 +50,22 @@ function loadRandomMeal(mealData, random = false) {
         <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
         <button class="btn btn-icon btn-next"><i class="fas fa-chevron-right fa-3x"></i></button>
     </div>
-    <div class="meal-body">
+    <div class="meal-body flex-center">
         <h4>${mealData.strMeal}</h4>
         <button class="btn btn-icon btn-add-fav fa-lg"><i class="far fa-heart"></i></button>
     </div>
     ` 
-    //add listener to that meal to check if added to fav
     meal.querySelector('.meal-header .btn-next').addEventListener('click', (e) => {
+        e.stopPropagation()
         getRandomMeal()
+        //add listener to that meal to check if added to fav
     })
     meal.querySelector('.meal-body .btn-add-fav').addEventListener('click', (e) => {
         //change heart icon
         if(e.target.classList.contains("far")){
             e.target.classList.remove("far")
             e.target.classList.add("fas")
+            e.stopPropagation()
             addToStorage(mealData.idMeal)
             getRandomMeal()
         } else {
@@ -69,10 +75,15 @@ function loadRandomMeal(mealData, random = false) {
         }
         fetchFavMeals()
     })
+    meal.addEventListener('click', () => {
+        showMealInfo(mealData)
+    })
+
     //add new div to meals list
     meals.appendChild(meal)
 }   
 
+// ============ GET RANDOM MEAL ============
 // ============== STORAGE ==============
 
 function getFromStorage() {
@@ -92,6 +103,7 @@ function removeFromStorage(mealId){
     localStorage.setItem('mealsIds', JSON.stringify(mealsIds.filter((id) => id !== mealId)))
 }
 
+// ============== STORAGE ==============
 // ============ FAVORITE MEALS ============
 
 //get ids from Storage and push obj to be added to fav
@@ -116,15 +128,20 @@ function addMealToFav(mealData) {
     ` 
     //delete button on favMeals
     const btn = favMeal.querySelector('.clear')
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation()
         removeFromStorage(mealData.idMeal)
 
-        fetchFavMeals()
+    fetchFavMeals()
     })
 
+    favMeal.addEventListener('click', () => {
+        showMealInfo(mealData)
+    })
+  
     favList.appendChild(favMeal)
 }
-
+// ============ FAVORITE MEALS ============
 // ================ SEARCH ================
 
 searchBtn.addEventListener('click', async () => {
@@ -153,7 +170,7 @@ function searchedMeals(mealData) {
     <div class="meal-header">
         <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
     </div>
-    <div class="meal-body">
+    <div class="meal-body flex-center">
         <h4>${mealData.strMeal}</h4>
         <button class="btn btn-icon btn-add-fav fa-lg"><i class="far fa-heart"></i></button>
     </div>
@@ -171,6 +188,56 @@ function searchedMeals(mealData) {
         }
         fetchFavMeals()
     })
+    meal.addEventListener('click', () => {
+        showMealInfo(mealData)
+    })
     //add new div to meals list
     meals.appendChild(meal)
 }   
+
+// ================ SEARCH ================
+// =============== MODAL ===============
+
+function showMealInfo(mealData) {
+    //clean up
+    modal.innerHTML = ''
+
+    //get ingredients
+    const ingredients = []
+    for (let i = 1; i < 21; i++){
+        if(mealData['strIngredient'+i]){
+            ingredients.push(`${mealData['strIngredient'+i]} - ${mealData['strMeasure'+i]}`)
+        } else {
+            break;
+        }
+    }
+
+    //show up modal by removing class hidden
+    modal.classList.remove('hidden')
+    //create modal
+    const recipe = document.createElement('div')
+    recipe.classList.add('meal-info')
+    recipe.innerHTML =`
+    <div class="info-body flex-center">
+    <h1>${mealData.strMeal}</h1>    
+    <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+    <p>${mealData.strInstructions}</p>
+    </div>
+    <div class="ingredients">
+    <h4>Ingredients:</h4>
+        <ul>
+            ${ingredients
+                .map(ing => `<li>${ing}</li>`)
+                .join('')}
+        </ul>
+    </div>
+    `
+    modal.appendChild(recipe)
+}
+
+//hide modal
+modal.addEventListener('click', () => {
+    modal.classList.add('hidden')
+})
+
+// =============== MODAL ===============
